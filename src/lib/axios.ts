c83ff3ec67d47f3d05c155b4e9572fb40de022b5
Rafael Galdino da Silva <rafaelgaldinosilva81@gmail.com>
2022-12-26 10:46:50 -0300

    fix: fixing url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // get current url from vercel env
-const current_url = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+const current_url =
+  process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000';
 const api = axios.create({
   baseURL: current_url,
 });
